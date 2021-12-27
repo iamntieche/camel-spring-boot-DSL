7018152c3db778d518ddcc9ea46a3f4ef1765cea
unknown <youssouf.tiefa@gmail.com>
2021-12-27 14:00:23 +0100

    Seda EIP implemented

--- camel/src/main/java/com/mfoumgroup/camel/component/SedaRoute.java
+++ camel/src/main/java/com/mfoumgroup/camel/component/SedaRoute.java
@@ -8,23 +8,41 @@ import org.apache.camel.support.DefaultMessage;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 @Component
+//@ConditionalOnProperty(name = "com.mfoumgroup.camel.seda.enabled", havingValue = "true")
 public class SedaRoute extends RouteBuilder {
+
+    static AtomicInteger counter = new AtomicInteger();
     @Override
-    public void configure() throws Exception {
+    public void configure() {
         from("timer:ping?period=200")
+                .routeId("Timer")
                 .process(this::currentDate)
                 //blocking queue with seda
-                .to("seda:weightLifter?multipleConsumers=true");
-        from("seda:weightLifter?multipleConsumers=true")
+               .to("seda:weightLifter?multipleConsumers=true");
+
+       from("seda:weightLifter?multipleConsumers=true")
+               .routeId("Seda-WeightLifter")
                 .to("direct:complexProcess");
 
         from("direct:complexProcess")
+                .routeId("Direct-ComplexProcess")
                 .log(LoggingLevel.DEBUG, "${body}")
-                .process(exchange -> SECONDS.sleep(2))
+                .process(exchange -> {
+                    if (new Random().nextInt(20) < 5) {
+                      System.err.println("error");
+                    }
+                })
+                .process(exchange -> {
+                    SECONDS.sleep(5);
+                    int total = counter.decrementAndGet();
+                    System.out.println("Counter: " + total);
+                })
                 .end();
     }
 
